3a9e04771c4dd3232077387d0504417f98a4f09d
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-09 15:59:30 +0000

    added renderers and dialogs for graduated symbol
    git-svn-id: http://svn.osgeo.org/qgis@445 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ branches/Datasource-branch/qgis/src/qgsgrasydialog.cpp
@@ -0,0 +1,235 @@
+/***************************************************************************
+                         qgsgrasydialog.cpp  -  description
+                             -------------------
+    begin                : Oct 2003
+    copyright            : (C) 2003 by Marco Hugentobler
+    email                : mhugent@geo.unizh.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgsgrasydialog.h"
+#include "qspinbox.h"
+#include "qpushbutton.h"
+#include <qcombobox.h>
+#include "qgsgrasyextensionwidget.h"
+#include "qgssymbologyutils.h"
+#include "qgsrangerenderitem.h"
+#include "qlineedit.h"
+#include "qgsgraduatedsymrenderer.h"
+#include "qgsshapefilelayer.h"
+#include "qgslegenditem.h"
+#include "qgslayerproperties.h"
+
+
+QgsGraSyDialog::QgsGraSyDialog(QgsShapeFileLayer* layer): QgsGraSyDialogBase(), ext(0), m_vectorlayer(layer)
+{
+    QObject::connect(numberofclassesspinbox,SIGNAL(valueChanged(int)),this,SLOT(adjustNumberOfClasses()));
+    QObject::connect(classificationComboBox,SIGNAL(activated(int)),this,SLOT(adjustNumberOfClasses()));
+    QObject::connect(closebutton,SIGNAL(clicked()),this,SLOT(hide()));
+    QObject::connect(applybutton,SIGNAL(clicked()),this,SLOT(apply()));
+    setOrientation(Qt::Vertical);
+
+    //Set the initial display name
+    displaynamefield->setText(m_vectorlayer->name());
+
+    //find out the numerical fields of m_vectorlayer
+    OGRLayer* l=m_vectorlayer->getOGRLayer();
+    l->SetSpatialFilter(0);
+    OGRFeature* f=l->GetNextFeature();
+    int numberoffields=f->GetFieldCount();
+    
+    for(int i=0;i<numberoffields;i++)
+    {
+	QString teststring=f->GetFieldAsString(i);
+	//test, if it is numeric or not (making a subclass of QString would be the proper solution)
+	bool containsletter=false;
+	for(uint j=0;j<teststring.length();j++)
+	{
+	    if(teststring.ref(j).isLetter())
+	    {
+	    containsletter=true;
+	    }
+	}
+
+	if(!containsletter)//add it to the map and to comboBox1 if it seems to be a numeric field
+	{
+	    QString str=f->GetFieldDefnRef(i)->GetNameRef();
+	    classificationComboBox->insertItem(str);
+	    //add teststring and i to the map
+	    m_fieldmap.insert(std::make_pair(str,i));
+	}
+    }
+    
+    modeComboBox->insertItem("equal interval");
+    modeComboBox->insertItem("empty");
+
+    setSizeGripEnabled(true);
+}
+
+QgsGraSyDialog::QgsGraSyDialog()
+{
+
+}
+
+QgsGraSyDialog::~QgsGraSyDialog()
+{
+    if(ext)
+    {
+	ext->hide();
+	delete ext;
+    }
+}
+
+void QgsGraSyDialog::adjustNumberOfClasses()
+{
+    showExtension(false);
+
+    hide();
+
+    //find out the number of the classification field
+    QString fieldstring=classificationComboBox->currentText();
+    std::map<QString,int>::iterator iter=m_fieldmap.find(fieldstring);
+    int field=iter->second;
+
+    //create a new extension dialog
+    if (modeComboBox->currentText()=="empty")
+    {
+	ext=new QgsGraSyExtensionWidget(this,field,QgsGraSyDialog::EMPTY,numberofclassesspinbox->value(),m_vectorlayer);
+    }
+    else if(modeComboBox->currentText()=="equal interval")
+    {
+	ext=new QgsGraSyExtensionWidget(this,field,QgsGraSyDialog::EQUAL_INTERVAL,numberofclassesspinbox->value(),m_vectorlayer);
+    }
+    show();
+
+    setExtension(ext);
+    showExtension(true);
+    
+}
+
+void QgsGraSyDialog::apply() const
+{
+    if(ext)
+    {
+	//create the pixmap for the render item
+	QPixmap* pix=m_vectorlayer->legendPixmap();
+	QString name=displaynamefield->text();
+	//query the name and the maximum upper value to estimate the necessary width of the pixmap (12 pixel width per letter seems to be appropriate)
+	int pixwidth=90+((QLineEdit*)(ext->getWidget(1,numberofclassesspinbox->value()-1)))->text().length()*2*12;//width of the pixmap with symbol and values
+	//consider 240 pixel for labels
+	int pixpluswidth=pixwidth+240;
+	int namewidth=45+name.length()*12;
+	int width=(pixpluswidth>namewidth) ? pixpluswidth : namewidth;
+	pix->resize(width,70+35*numberofclassesspinbox->value());
+	pix->fill();
+	QPainter p(pix);
+
+	//draw the layer name and the name of the classification field into the pixmap
+	p.drawText(45,35,name);
+	m_vectorlayer->setlayerName(name);
+	p.drawText(45,70,classificationComboBox->currentText());
+
+	((QgsGraduatedSymRenderer*)(m_vectorlayer->renderer()))->removeItems();
+
+	for(int i=0;i<numberofclassesspinbox->value();i++)
+	{
+	    QgsSymbol sy(QColor(255,0,0));
+
+	    if(m_vectorlayer->vectorType()==QgsShapeFileLayer::Polygon)
+	    {
+		sy.pen().setColor(((QPushButton*)(ext->getWidget(3,0)))->paletteBackgroundColor());
+		sy.pen().setStyle(QgsSymbologyUtils::qString2PenStyle((((QPushButton*)(ext->getWidget(4,0)))->text())));
+		sy.pen().setWidth(((QSpinBox*)(ext->getWidget(5,0)))->value());
+	    }
+	    else
+	    {
+		sy.pen().setColor(((QPushButton*)(ext->getWidget(3,i)))->paletteBackgroundColor());
+		sy.pen().setStyle(QgsSymbologyUtils::qString2PenStyle((((QPushButton*)(ext->getWidget(4,i)))->text())));
+		sy.pen().setWidth(((QSpinBox*)(ext->getWidget(5,i)))->value());
+	    }
+
+	    if(m_vectorlayer->vectorType()!=QgsShapeFileLayer::Line)
+	    {
+		sy.brush().setColor(((QPushButton*)(ext->getWidget(6,i)))->paletteBackgroundColor());
+	    }
+
+	    if(m_vectorlayer->vectorType()==QgsShapeFileLayer::Polygon)
+	    {
+		sy.brush().setStyle(QgsSymbologyUtils::qString2BrushStyle((((QPushButton*)(ext->getWidget(7,i)))->text())));
+	    }
+	    else if (m_vectorlayer->vectorType()==QgsShapeFileLayer::Point)
+	    {
+		sy.brush().setStyle(Qt::SolidPattern);
+	    } 
+
+	    QString lower_bound=((QLineEdit*)(ext->getWidget(0,i)))->text();
+	    QString upper_bound=((QLineEdit*)(ext->getWidget(1,i)))->text();
+	    QString label=((QLineEdit*)(ext->getWidget(2,i)))->text();
+
+	    //test, if lower_bound is numeric or not (making a subclass of QString would be the proper solution) 
+	    bool lbcontainsletter=false;
+	    for(uint j=0;j<lower_bound.length();j++)
+	    {
+		if(lower_bound.ref(j).isLetter())
+		{
+		    lbcontainsletter=true;
+		}
+	    }
+
+            //test, if upper_bound is numeric or not (making a subclass of QString would be the proper solution)
+	    bool ubcontainsletter=false;
+	    for(uint j=0;j<upper_bound.length();j++)
+	    {
+		if(upper_bound.ref(j).isLetter())
+		{
+		    ubcontainsletter=true;
+		}
+	    }
+
+	    if(lbcontainsletter==false&&ubcontainsletter==false&&lower_bound.length()>0&&upper_bound.length()>0)//only add the item if the value bounds do not contain letters and are not null strings
+	    {
+		QgsRangeRenderItem* item = new QgsRangeRenderItem(sy, lower_bound, upper_bound, ((QLineEdit*)(ext->getWidget(2,i)))->text());
+		((QgsGraduatedSymRenderer*)(m_vectorlayer->renderer()))->addItem(item);
+	    
+		//add the symbol to the picture
+	    
+		QString legendstring=lower_bound+" - "+upper_bound;
+		p.setPen(sy.pen());
+		p.setBrush(sy.brush());
+		if(m_vectorlayer->vectorType()==QgsShapeFileLayer::Polygon)
+		{
+		    p.drawRect(10,70+5+30*i,30,20);//implement different painting for lines and points here
+		}
+		else if(m_vectorlayer->vectorType()==QgsShapeFileLayer::Line)
+		{
+		    p.drawLine(10,70+5+30*i+10,40,70+5+30*i+10);
+		}
+		else if(m_vectorlayer->vectorType()==QgsShapeFileLayer::Point)
+		{
+		    p.drawRect(25,70+5+30*i+10,5,5);
+		}
+		p.setPen(Qt::black);
+		p.drawText(45,70+25+30*i,legendstring);
+		p.drawText(pixwidth+10,70+25+30*i,label);
+	    }
+	}
+
+	((QgsGraduatedSymRenderer*)(m_vectorlayer->renderer()))->setClassificationField(ext->classfield());
+	m_vectorlayer->triggerRepaint();
+	m_vectorlayer->legendItem()->setPixmap(0,(*pix));
+    }
+
+    else//number of classes is 0
+    {
+	std::cout << "warning, number of classes is zero" << std::endl << std::flush;
+    }
+
+}
