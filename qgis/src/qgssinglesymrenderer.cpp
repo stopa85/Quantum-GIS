5b0e5f646ad28f4b2119bf1c075af64ed05dbfcd
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-16 23:06:24 +0000

    Changed size of legend pixmap and font for layer name
    git-svn-id: http://svn.osgeo.org/qgis/branches/Datasource-branch@498 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- qgis/src/qgssinglesymrenderer.cpp
+++ qgis/src/qgssinglesymrenderer.cpp
@@ -14,7 +14,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-
+/* $Id$ */
 #include "qgssinglesymrenderer.h"
 #include "qgsfeature.h"
 #include "qgis.h"
@@ -216,17 +216,17 @@ void QgsSingleSymRenderer::initializeSymbology(QgsVectorLayer* layer)
 	QString name=layer->name();
 	int namewidth=10+name.length()*12;//12 pixel per letter seems appropriate
 	int width=(namewidth>60) ? namewidth : 60;
-	pixmap->resize(width,75);
+	pixmap->resize(width,35);
 	pixmap->fill();
 	QPainter p(pixmap);
-	p.drawText(10,35,name);
+	
 
 	if(layer->vectorType()==QGis::Line)
 	{
 	    sy.pen().setColor(QColor(red,green,blue));
 	    //paint the pixmap for the legend
 	    p.setPen(sy.pen());
-	    p.drawLine(10,55,40,55);
+	    p.drawLine(10,10,40,55);
 	}
 	else
 	{
@@ -237,14 +237,19 @@ void QgsSingleSymRenderer::initializeSymbology(QgsVectorLayer* layer)
 	    p.setBrush(sy.brush());
 	    if(layer->vectorType()==QGis::Point)
 	    {
-		p.drawRect(20,55,5,5);
+		p.drawRect(20,10,5,5);
 	    }
 	    else//polygon
 	    {
-		p.drawRect(10,45,30,20);
+		p.drawRect(10,10,20,15);
 	    }
 			
 	}
+  // set the font for the legend text
+  //TODO Make the font size a user option
+   QFont f( "times", 12, QFont::Normal );
+  p.setFont( f );
+  p.drawText(35,20,name);
 	QgsRenderItem ri(sy,"", "");
 	addItem(ri);
 
