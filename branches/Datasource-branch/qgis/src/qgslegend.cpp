befd515d96b7efc329968bb34ecfd39275d8af4d
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-16 23:08:34 +0000

    Added $Id$ Keyword expansion
    git-svn-id: http://svn.osgeo.org/qgis@499 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- branches/Datasource-branch/qgis/src/qgslegend.cpp
+++ branches/Datasource-branch/qgis/src/qgslegend.cpp
@@ -15,6 +15,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
+ /* $Id$ */
 #include <map>
 #include <qstring.h>
 #include <qpainter.h>
