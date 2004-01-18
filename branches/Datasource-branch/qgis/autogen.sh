82f12c143e1bb883b5ef1a9720351e4472bc59da
gsherman <gsherman@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2004-01-18 07:27:30 +0000

    fixed script to fail gracefully
    git-svn-id: http://svn.osgeo.org/qgis@511 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- branches/Datasource-branch/qgis/autogen.sh
+++ branches/Datasource-branch/qgis/autogen.sh
@@ -3,5 +3,5 @@ echo Configuring build environment for QGIS
 aclocal  \
   && libtoolize --force --copy \
   && automake --add-missing --foreign --copy \
-  && autoconf
-echo Now run configure to configure QGIS
+  && autoconf \
+  && echo Now run configure to configure QGIS
