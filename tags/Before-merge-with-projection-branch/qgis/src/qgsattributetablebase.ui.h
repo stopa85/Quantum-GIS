531c4442f5de07e265a50bc2569e0eaa1b0f85ea
(no author) <(no author)@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-04-08 23:42:50 +0000

    This commit was manufactured by cvs2svn to create tag 'Before-merge-with-projection-branch'.
    git-svn-id: http://svn.osgeo.org/qgis@3103 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tags/Before-merge-with-projection-branch/qgis/src/qgsattributetablebase.ui.h
@@ -0,0 +1,30 @@
+void QgsAttributeTableBase::closeEvent(QCloseEvent* ev)
+{
+    ev->ignore();
+    emit deleted();
+    delete this;
+}
+
+
+void QgsAttributeTableBase::deleteAttributes()
+{
+
+}
+
+
+void QgsAttributeTableBase::addAttribute()
+{
+
+}
+
+
+void QgsAttributeTableBase::startEditing()
+{
+
+}
+
+
+void QgsAttributeTableBase::stopEditing()
+{
+
+}
