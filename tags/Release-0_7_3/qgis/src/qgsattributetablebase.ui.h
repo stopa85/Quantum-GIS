99228058039db763a960a012117e874eda64fa45
(no author) <(no author)@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2005-10-11 00:04:23 +0000

    This commit was manufactured by cvs2svn to create tag 'Release-0_7_3'.
    git-svn-id: http://svn.osgeo.org/qgis@4064 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ tags/Release-0_7_3/qgis/src/qgsattributetablebase.ui.h
@@ -0,0 +1,48 @@
+void QgsAttributeTableBase::closeEvent(QCloseEvent* ev)
+{
+    ev->ignore();
+    emit deleted();
+    delete this;
+}
+
+
+void QgsAttributeTableBase::deleteAttributes()
+{
+
+}
+
+
+void QgsAttributeTableBase::addAttribute()
+{
+
+}
+
+
+void QgsAttributeTableBase::startEditing()
+{
+
+}
+
+
+void QgsAttributeTableBase::stopEditing()
+{
+
+}
+
+
+void QgsAttributeTableBase::invertSelection()
+{
+
+}
+
+
+void QgsAttributeTableBase::selectedToTop()
+{
+
+}
+
+
+void QgsAttributeTableBase::removeSelection()
+{
+
+}
