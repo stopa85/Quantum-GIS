3fa20757e95786fbaf0f5dd9bf6d41c2efbc3b9f
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-01-25 08:14:40 +0000

    Recreated diagram-branch from svn head
    git-svn-id: http://svn.osgeo.org/qgis@6455 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ branches/diagram-branch/src/plugins/plugin_template/plugingui.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tim Sutton                                      *
+ *   tim@linfiniti.com                                                     *
+ *                                                                         *
+ *   This is a plugin generated from the QGIS plugin template              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+#include "[pluginlcasename]gui.h"
+
+//qt includes
+
+//standard includes
+
+[pluginname]Gui::[pluginname]Gui() : [pluginname]GuiBase()
+{
+  setupUi(this);
+}
+
+[pluginname]Gui::[pluginname]Gui( QWidget* parent, Qt::WFlags fl )
+: QDialog ( parent, fl )
+{
+  setupUi(this);
+}  
+[pluginname]Gui::~[pluginname]Gui()
+{
+}
+
+void [pluginname]Gui::pbnOK_clicked()
+{
+  //
+  // If you have a produced a raster layer using your plugin, you can ask qgis to 
+  // add it to the view using:
+  // emit drawRasterLayer(QString("layername"));
+  // or for a vector layer
+  // emit drawVectorLayer(QString("pathname"),QString("layername"),QString("provider name (either ogr or postgres"));
+  //
+  //close the dialog
+  done(1);
+} 
+void [pluginname]Gui::pbnCancel_clicked()
+{
+ close(1);
+}
