df4f5a22196c8d2e0c3020a85f38a3499cae70d4
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2007-12-15 17:41:39 +0000

    First framework for transformations of project file versions
    git-svn-id: http://svn.osgeo.org/qgis/branches/qgis_rev-up@7790 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/core/qgsprojectfile.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+                          qgsprojectfile.cpp  -  description
+                             -------------------
+    begin                : Sun 15 dec 2007
+    copyright            : (C) 2007 by Magnus Homann
+    email                : magnus at homann.se
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ /* $Id: qgsprojectfile.cpp 6942 2007-05-13 13:40:35Z mhugent $ */
+
+
+#include "qgsprojectfile.h"
+#include <QTextStream>
+#include <QDomDocument>
+
+typedef QgsProjectFileVersion PFV;
+
+
+QgsProjectFile::transform QgsProjectFile::transformers[] = {
+  {PFV(0,8,1), PFV(0,9,0), &QgsProjectFile::transform081to090},
+  {PFV(0,9,0), PFV(0,9,1), &QgsProjectFile::transform090to091},
+  {PFV(0,9,1), PFV(0,9,2), &QgsProjectFile::transformNull}
+};
+
+bool QgsProjectFile::revup(QgsProjectFileVersion newVersion)
+{
+  bool returnValue = false;
+
+  if ( ! mDom.isNull() )
+  {
+    for (int i = 0; i < sizeof(transformers)/sizeof(transform); i++)
+    {
+      if ( transformers[i].from == mCurrentVersion)
+      {
+        // Run the transformer, and update the revision in every case
+        (this->*(transformers[i].transformFunc))();
+        mCurrentVersion = transformers[i].to;
+        returnValue = true;
+      }
+    }
+  }
+  return returnValue;
+};
+
+void QgsProjectFile::transform081to090()
+{
+  if ( ! mDom.isNull() )
+  {
+  }
+  return;
+
+};
+
+void QgsProjectFile::transform090to091()
+{
+  if ( ! mDom.isNull() )
+  {
+  }
+  return;
+
+};
