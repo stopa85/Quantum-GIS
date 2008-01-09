6c47a80df6d372c026082c64472ee6267a522e24
homann <homann@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-01-09 19:33:27 +0000

    Updated QgsProjectVersion
    git-svn-id: http://svn.osgeo.org/qgis/branches/qgis_rev-up@7891 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/core/qgsprojectversion.h
+++ src/core/qgsprojectversion.h
@@ -28,7 +28,7 @@ class QgsProjectVersion
 
   QgsProjectVersion() {} 
 
-  QgsProjectVersion(int major, int minor, int sub);
+  QgsProjectVersion(int major, int minor, int sub, QString name = "");
 
   QgsProjectVersion(QString string);
 
@@ -37,6 +37,8 @@ class QgsProjectVersion
   int major() { return mMajor;};
   int minor() { return mMinor;};
   int sub()   { return mSub;};
+
+  QString text();
 
   /*! Boolean equal operator
    */
@@ -55,6 +57,7 @@ class QgsProjectVersion
   int mMajor;
   int mMinor;
   int mSub;
+  QString mName;
 };
 
 #endif // QGSPROJECTVERSION_H
