8f8f150e1752c828618cf19565bf8ac75bc65d62
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2008-10-24 11:49:51 +0000

    Add missing files to svn
    git-svn-id: http://svn.osgeo.org/qgis/branches/advanced_printing_branch2@9531 c8812cc2-4d05-0410-92ff-de0c093fc19c

---
+++ src/core/composer/qgspaperitem.cpp
@@ -0,0 +1,88 @@
+/***************************************************************************
+                         qgspaperitem.cpp
+                       -------------------
+    begin                : September 2008
+    copyright            : (C) 2008 by Marco Hugentobler
+    email                : marco dot hugentobler at karto dot baug dot ethz dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgspaperitem.h"
+#include <QPainter>
+
+QgsPaperItem::QgsPaperItem(QgsComposition* c): QgsComposerItem(c, false)
+{
+  setZValue(0);
+}
+
+QgsPaperItem::QgsPaperItem( qreal x, qreal y, qreal width, qreal height, QgsComposition* composition ): QgsComposerItem(x, y, width, height, composition, false)
+{
+  setZValue(0);
+}
+
+QgsPaperItem::QgsPaperItem(): QgsComposerItem(0, false)
+{
+
+}
+
+QgsPaperItem::~QgsPaperItem()
+{
+  
+}
+
+void QgsPaperItem::paint( QPainter* painter, const QStyleOptionGraphicsItem* itemStyle, QWidget* pWidget )
+{
+  if ( !painter )
+  {
+    return;
+  }
+
+  drawBackground( painter );
+
+  //draw grid
+
+  if(mComposition)
+    {
+      if(mComposition->snapToGridEnabled() && mComposition->plotStyle() ==  QgsComposition::Preview 
+	 && mComposition->snapGridResolution() > 0)
+	{
+	  QPen pen(QColor(100, 100, 100));
+	  pen.setWidthF(0.3);
+	  painter->setPen(pen);
+
+	  //draw vertical lines  
+	  int gridMultiplyX = (int)(mComposition->snapGridOffsetX() / mComposition->snapGridResolution());  
+	  double currentXCoord = mComposition->snapGridOffsetX() - gridMultiplyX * mComposition->snapGridResolution();
+	  for(; currentXCoord <= rect().width(); currentXCoord += mComposition->snapGridResolution())
+	    {
+	      painter->drawLine(QPointF(currentXCoord, 0), QPointF(currentXCoord, rect().height()));
+	    }
+	  
+	  //draw horizontal lines
+	  int gridMultiplyY = (int)(mComposition->snapGridOffsetY() / mComposition->snapGridResolution());
+	  double currentYCoord = mComposition->snapGridOffsetY() - gridMultiplyY * mComposition->snapGridResolution();
+	  for(; currentYCoord <= rect().height(); currentYCoord += mComposition->snapGridResolution())
+	    {
+	      painter->drawLine(QPointF(0, currentYCoord), QPointF(rect().width(), currentYCoord));
+	    }
+	}
+    }
+}
+
+bool QgsPaperItem::writeXML( QDomElement& elem, QDomDocument & doc )
+{
+  return true;
+}
+
+bool QgsPaperItem::readXML( const QDomElement& itemElem, const QDomDocument& doc )
+{
+  return true;
+}
