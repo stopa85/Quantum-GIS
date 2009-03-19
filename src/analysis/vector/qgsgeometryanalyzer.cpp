86dbfa667f1a055cffbb4741bd9b94c165e3c355
timlinux <timlinux@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-03-19 18:08:01 +0000

    Added standard header to analyzer cpp file
    git-svn-id: http://svn.osgeo.org/qgis/branches/analysis_branch@10307 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/analysis/vector/qgsgeometryanalyzer.cpp
+++ src/analysis/vector/qgsgeometryanalyzer.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+    qgsgeometryanalyzer.cpp - QGIS Tools for vector geometry analysis
+                             -------------------
+    begin                : 19 March 2009
+    copyright            : (C) Carson Farmer
+    email                : carson.farmer@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id: qgis.h 9774 2008-12-12 05:41:24Z timlinux $ */
 bool QgsGeometryAnalyzer::single_to_multi( self )
 {
 /*    vprovider = self.vlayer.dataProvider()
