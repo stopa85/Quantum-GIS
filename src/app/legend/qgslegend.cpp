08c89eb19e1eb9dc83178c0a16706c8c1a800726
mhugent <mhugent@c8812cc2-4d05-0410-92ff-de0c093fc19c>
2009-06-10 07:39:56 +0000

    Backport of legend cursor fix to 1.0 branch
    git-svn-id: http://svn.osgeo.org/qgis/branches/Version-1_0@10898 c8812cc2-4d05-0410-92ff-de0c093fc19c

--- src/app/legend/qgslegend.cpp
+++ src/app/legend/qgslegend.cpp
@@ -266,6 +266,7 @@ void QgsLegend::mouseMoveEvent( QMouseEvent * e )
               moveItem( dest, origin );
             }
           }
+          setCursor( QCursor( Qt::SizeVerCursor ) );
           setCurrentItem( origin );
         }
         else
@@ -290,6 +291,7 @@ void QgsLegend::mouseMoveEvent( QMouseEvent * e )
             //origin->moveItem(dest);
             moveItem( origin, dest );
           }
+          setCursor( QCursor( Qt::SizeVerCursor ) );
           setCurrentItem( origin );
         }
         else if ( action == QgsLegendItem::INSERT )
